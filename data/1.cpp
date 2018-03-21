d3c3f0284feb51f01ce528e25279b3efd596870f
Tianyi Chen <TianyiChen@users.noreply.github.com>
2018-03-21 12:45:59 -0500

    Create data

---
+++ data/1.cpp
@@ -0,0 +1,731 @@
+int zWre
+,	a0pU ,
+  	FaiS,	nNp/*UJGq*/,
+Rg//Zc
+,	
+UWu 	 ,	sl ,//T
+
+Kt0
+,
+//h
+vfZ  ,
+
+jw,	 j56y , V3H
+
+, HN ,WkzG3BH
+ 	 ,Kw, 
+fJ8	,	W,
+xT5	,OsCi//dl
+, dUv/*k*/ ,
+cmoG  ,	jJ,beF,WTG, b
+ ,
+YcEa3;void f_f0//yx
+	(){
+ volatile int
+
+ rd,//lD3h
+xzw, eScc6 
+,F5,	
+q3v; 
+YcEa3
+=	q3v//4yXi
+ + rd	+	xzw +	 eScc6
++F5;; 	{
+
+
+{{  {}		}
+{volatile int Z ,	cc2 ,a6H0
+,	Xx,qa ;
+
+for	(int i=1; i<
+1
+ ;++i )for	(int 
+i=1;i<
+2;++i/*7M1q*/
+)zWre	 = qa+	
+Z
++
+cc2/*S*/+a6H0 	 +Xx	//UsNY
+ ;
+}} {for(int 		i=1
+;
+		
+
+i<3
+;++i 
+ ) if	(
+true)  
+for(int 
+i=1	;	 i<	4;++i	){//GOa
+//f2
+ int
+
+ uivB 
+;volatile int	  oa
+ ,	MR,	Ylnh 	;/*9mD*/uivB
+
+=Ylnh
++	/**/oa
++	MR
+ ;}else	{	for (int i=1 ;i<5 ;++i
+
+	)
+{ 
+ }if
+(true)/*sEY*/for/**/(int 	 i=1	;	
+
+i< 6
+;++i) 
+{	
+}else if(
+
+ true)	//7on
+{{} }/*h6*/else{
+volatile int X7Oj/*Z*/	,	H6Ay //
+//u
+	;
+
+ a0pU
+=	
+H6Ay+
+X7Oj
+;}
+}
+ {
+if(true 
+){;//9F
+}else
+{		
+ if/*p*/(/*f*//*z*/true
+ )/*LW*/; else//qk
+{for
+ 	(int 	
+i=1;i< 7;++i 
+)return 
+/*WFFi*/ ;	{}
+	return ;	
+} /*eQ*/}
+{}} if (
+ true	)
+{;
+{ }}  else	{{
+}
+}} {/*e*/{;
+ 	{{}{	;} }
+
+} {int sqib;volatile int  K
+
+,	YIt ,	 	 b1 ,
+nfgJ ; {int A
+;
+ 	volatile int KD3/*SO*/,XGrA	  
+;for(int i=1	;/*9*/i<	8;++i
+){  }{
+}	if(	true)//IW
+
+A =  XGrA+
+ KD3 ;else 	 return ; 	}
+ ;
+sqib
+=/*qs*/nfgJ
+
+
++	K+YIt
+
+	+b1	;}
+{		volatile int QxWFELh ,
+
+cCori9 ;//R
+ //EZ
+{;
+
+}for	(int 
+	i=1;i<
+9	;++i	)
+FaiS=	cCori9
++
+
+QxWFELh	
+;
+ for(int i=1 ;i<	10 ;++i/*Q*/) if	
+(
+true)/*i*/	if(true	) if(
+true)return ; else for	
+(int i=1
+;	i< 		11	 ;++i){	{
+	;}//0C
+	} else return ; else {
+if  	(
+true
+ ){ }
+else {}/*pd*/{if(	true ){//
+}	
+else  {  //r5N
+	  }  }	}}  return ; }
+ }{
+for	(int i=1;
+i<
+	12
+;++i  	);{{{}
+}
+	
+
+
+{ 
+if	(true) return ;
+else if( true
+
+){}	 	else{{
+ 	} }{	 volatile int
+ qGDS	 ;	return/*Gm*/	 ;nNp=
+qGDS
+;}if	(
+true
+)return ; else if//LUy
+(	true){}
+ else ;} {volatile int
+ RO,//hS
+ SFM,	LN;;
+return				 ;if	 ( 
+true 	)  Rg=	LN/*tD*/	+
+
+RO+SFM;else for (int 
+i=1//h
+; i< 13/*KeRO*/;++i//X1
+
+
+)return
+ ;} { {}{/*u*/{	}	}}return
+  ;	}
+{
+{/*R*/
+int   juv ;volatile int Xj  //J
+,
+	n;return
+
+ ;{ 	{{ }} {
+if(true
+) 	{
+ }	 else
+ { 	
+}
+ }	}	 {}juv= n	
+/*k*/+Xj	 ;	}	;	return  ;	/*wVp*/ }}	  return 
+ ;}
+void  f_f1 ()	{int/*7*/ RK8h
+;	 
+volatile int	 AiK,
+K5l,UC	/*SU*/,	DK		,	m6n
+
+;{ 	int
+  b9L 	; volatile int sS,
+D		,	tbh,	/*av*/fNR5t , RutGd		,BH
+,kCS//yut9v
+,	FYc
+	,qP;	return ;	UWu=qP+
+sS	+
+D+tbh+ fNR5t ; {  volatile int zJt	, FMX,miVTB
+ ,/*dDp*/	
+ Pt 	,
+OeZ//y1B
+, FM8PG	,	wf,Icck,/*O3N*//*g*/CCey14
+;	 {
+ {} //9TBS
+//Cfz
+if
+
+ 
+(true
+)	
+{
+
+{
+		}/*CG9*/ {} {{
+};	
+
+}
+ 	
+} 	else	 if  (/*Mc*/
+true
+){}else return	/*C*/	 ;//e
+{ {volatile int X;
+{ 	}if(true
+){}else	 if	//rj
+(true)for (int i=1
+;//
+i<14
+;++i/*21*/)//
+{
+		 }  else//dBRyTD
+{}sl= X //L0
+; }} 	}  Kt0= CCey14
+
+ + 
+	
+ zJt+ /*mW*/FMX	+miVTB
++Pt
+;	vfZ =OeZ	+
+FM8PG
++
+wf +
+Icck	;
+ 	}		b9L	
+=RutGd	+//u
+BH+ kCS	+	FYc 	;}
+ {  	int ZXo//l
+,EyMW ;volatile int  RA,OX,
+yfe, FAH,OrV		,
+gzJF	
+ ,yYdBx,Mw,	Wuv    ; 
+	for/**/ (int 
+i=1;
+
+i<
+
+
+15	 ;++i)
+{//Gm
+
+int dI7Y3
+;	volatile int yb , FkO ,Cp;for	//oR
+(int 	
+i=1
+; i<16 ;++i )if	(	true
+)return ;else	
+{ 
+;}dI7Y3=Cp+
+yb
++FkO;{	return  ;
+}}
+if
+(
+true//H4
+){ ;{//
+/*avsbb*/
+return	
+/*KT*///gt8
+
+ ;//JEIXP
+
+{	;return ;//xz0
+	} { 	{}
+}/**/}/*Kh6*/}//
+	
+ else
+ 
+ EyMW
+
+	=Wuv
+
++
+
+			RA+	OX+yfe; return	  ;ZXo	=FAH	 +OrV +	gzJF+yYdBx	+Mw   	;	
+	}	RK8h=	m6n+	AiK	//f
++K5l+UC 
++
+DK ;;
+
+{int  s1jRR	; 
+volatile int	 qa5	
+,Newn,	cG,ODrt
+,Vym
+,E,N,	Hc,//i6nu
+m9p 	;{{
+ {
+for 
+(int 
+i=1 ;
+i<17;++i);{}
+} }/*Q*/	/*E*/{if(
+true)
+
+{	return  //g
+ ;}
+else
+
+ for (int  
+i=1	;	i<18
+;++i	)	for(int i=1;	/*B*/i<  19	;++i
+)	{	if	(
+true){}else
+ return		
+ ;/*2j*//*o*/ {  {
+ }}}{
+return
+ ;
+if(true	)	 	
+{ {
+	}{}	}else	
+{{}
+		}
+
+{	
+{}} } } }{
+	volatile int  Sz5 	,E4	,
+YU,	WWo ; 
+jw  
+=WWo +
+Sz5	+
+E4  +YU
+	;
+;{ //od
+{ if 	
+(	/**/true  )//UVl
+
+{
+{ }}
+
+		else{}};}//
+}if(	true	 ) 	for (int 	i=1;i<
+20//aNR
+	;++i	)for	(int i=1
+
+		;i<21	
+ ;++i) 
+	if
+
+
+(
+true
+)	j56y=m9p	+qa5 
++	Newn
+
++cG
+
+; else return ;
+else	 for(int  i=1;
+i<	22;++i)	return  ;{	for	(int //hz
+i=1//ib
+;i<	 23;++i	)	{
+
+;
+{}
+/*QF4*///V
+}	return ;{{}
+/*9*/}	 }s1jRR=ODrt
++Vym	 +//
+E	
+ + N+ 
+
+Hc;}	return ;		} void  f_f2() {
+ {
+return  
+ ;//lnK
+	;	 {	
+
+{	return ;{	} 	} ;} 
+ }/*A8nq*/return ;{
+{{ {{  { } };
+{
+}	}} for(int 		i=1
+; 		i<		
+24 
+	;++i)
+{ 
+ volatile int
+	 LC /*4*/	,w2m, Z3; 
+{	{} 
+}for (int 
+i=1;
+i<25 ;++i)
+ { {		}
+return  ;	}	/*0kqvp*/V3H
+=Z3+//boQhG
+		LC	+	w2m	 ;}
+
+}  {	
+volatile int DQAS	//t0I
+,
+kT5o, T3XF,		SLS 	,a;
+HN
+= a+	DQAS	+
+	kT5o+	T3XF	+	
+SLS
+
+;	 for	(int  i=1;	 i<
+ 26
+
+;++i	)
+if( 	true)/**/{ { int gvJ;volatile int vA, j	//
+;for  (int i=1
+;	i</*O1g*/ 27//H
+;++i//
+//
+)gvJ	=j	+	vA ;		} }
+
+else
+	 {{{ }/*Bah47*/}
+return/*ao*/ ;
+}/*Gj9*/{//O6
+
+if
+(
+true ){{}	}else  if(true)
+return	 ;else	{
+{ }	}	{if( true)for
+(int i=1	;i<28 ;++i	)return ;else ;
+{//q
+	}for/*qB*/ (int 
+//BpItL
+i=1	;i<29	;++i) for  (int  i=1  ;   i<	30;++i/*Swq*/)	
+{ 	}}for(int i=1; i<31;++i
+
+)		{	{ int/*fnvZ*/ P9
+  ; volatile int   YlZ ;
+	
+{}P9=//S0
+YlZ
+ ;
+};	
+ 
+} {}//ehPUC
+}}return
+
+ ;
+	{
+{return
+ ;	;
+}
+ {{	//zbp
+{}
+/*9*/;		}}}}  { 	{	 	{{
+if
+( 	
+true	){		 }else{
+//F
+{}/*7n*/	for(int  i=1 ;i<32;++i) {} }
+//
+{
+ }
+ {
+}//FC5
+}
+{}}
+ {
+ 
+for		(int //
+i=1;i<	
+33;++i 
+)
+{}}
+} {  if
+( true)	if	
+ (	true){	 volatile int sV5y
+,
+	Ax,
+zxo,
+r6C	;WkzG3BH =	 r6C 
++ sV5y //1m
++Ax	+		zxo 
+;	 if(true
+){	}	else
+ return
+ ;	} else{{ volatile int kgIw	,SDRJ,bTQXUE
+;//zZ7p3
+if	(true	)if (true
+)
+{ 
+/*QT*/}else Kw=bTQXUE;
+ else fJ8//i
+=	 kgIw//TNp
++
+SDRJ;}
+ ;		return ;//h
+	
+}//P
+else return
+ ;{{//arn7R
+int//XH
+ E6 	;volatile int  ITcB,HD	
+,
+ JlI2	 ;E6 =
+	
+JlI2	  + ITcB   +HD
+;}
+	} 	}
+for//p
+
+(int  i=1
+;i<
+  	34	
+;++i	)/*81rL*/
+;
+
+ {//8
+	{/*j*/;//z
+{ 
+return ;}} 	 {
+{	 }		}	;	
+}}{volatile int
+
+ Q6
+
+
+,	iM		, N0bv
+,BMQ
+,   x,ZYPZ	, d9  ,
+/*D*/Z97/*M*/,
+d,/*EAj*/GNt
+,oNNO ;
+W	= oNNO
+ +Q6+iM+ N0bv	 +
+BMQ+	x;
+xT5 =	ZYPZ+	d9
++ Z97//T3R
+	+
+/*y*/ d+
+GNt ;{//
+{{
+
+{ int KPWtCP	/**/;volatile int pa
+
+
+
+;{}
+{}KPWtCP=	
+pa	;		}	} 	{if	
+	(/*50*/true 	)for		(int 	i=1;i<	35 ;++i ) return ;
+else	{
+ return
+ ;}	 } } for	(int 
+i=1;i<
+
+	36;++i){for	(int 
+
+i=1
+	/*7*/;	 i<37;++i
+/*mv*/	)		 ;}}{{ /*EP*/{{	 } 	}		{	if
+	(
+true
+)	{	{ }}else	
+ return	 ; 	; }} ;{ 	int
+	  fV ;/*KT*/volatile int//5J
+ pEc4,	//c
+	P2yq, eIvCo 	; 	if 	(true	)	fV	=eIvCo+
+pEc4 +P2yq 	
+;else  {{/**/volatile int
+ NF 	
+;OsCi= //X
+NF;
+}
+ 
+}for(int 
+
+i=1;
+ i<38 ;++i
+ ){		}/*Y*/}} }{
+volatile int ug4k,ainA,eneX
+,	E1Q ,oQT ,cv
+; 
+return/*r*/
+ ;
+if(true
+)//R
+dUv
+	=cv +	
+	ug4k
+ 	+
+ainA+
+
+eneX	+E1Q +
+oQT/*lh8q*/;	else{	{		;	} {	{{
+} }
+{	
+int c//7bK
+ //h1h
+;volatile int  Ew,gKEv ,	 RFPu0;return	  ;{ } 
+cmoG	= RFPu0//4be6
+;
+c  =Ew+gKEv;  }	 ; };{{ volatile int	 FSn;
+{
+}	  		for 
+(int i=1;	i<	39 ;++i
+	)
+	 jJ	=FSn
+;//e1n
+}	} ;}
+ for
+(int 		i=1; i<
+40
+;++i	)	{	
+{{	volatile int
+/**/ KRDzC//GKv
+,u4;
+{;
+}
+ 
+	beF	=
+u4 +	KRDzC	//
+ //tp
+;	
+}{	;
+}
+} 	{ {{
+}//dfYPn
+  if(	true	)for (int i=1 ;i<41;++i )	
+{ }	else{//1
+ }}}
+ }}	return
+
+/*l*/ ;}
+int main
+ () {	volatile int
+ vj/*PIO0*/
+,
+ oDIs  //LK
+
+, E9KdN	,	GV,	//5
+vN,  xMu;
+for	(int 		i=1	;	i< 	
+ 42;++i)
+
+WTG = xMu 	+vj//mwE1
++oDIs
+
++
+
+E9KdN +GV
+	
++	
+
+vN ;/*16rhd*/return  1740126450 	; 	{{return
+ 857809738;	
+;{
+{/*FDL*/}	
+}
+
+
+}	return	 40088754
+ ; {	;	;
+
+}	{ 
+volatile int
+ o	
+,rhXn	//FZGU
+,ew1L;/*Zgp1G*/b //l6
+/*o*/	=ew1L 	+o 	+
+rhXn  ; for 	
+(int 	i=1 /*YG*/;i<  	43//a
+;++i//e
+)for
+
+(int i=1	;//Y
+ 	i<44;++i)	{
+int YlN ;volatile int		 dlK, 	kTUoN, gMk, yL4 ,U 
+;	{//KSmx
+	{ }} YlN
+=	U+	 dlK +kTUoN	//Y
++gMk+yL4
+;}{ 	if(
+true){
+
+;	
+} else
+{	
+//0W
+{ 
+{		}}	
+}	}
+
+} } 
+return 1621589056; }
